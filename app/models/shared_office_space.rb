65b63bc9ae98de25bcafbe0dd926132d39b00240
Santosh Wadghule <santosh.wadghule@gmail.com>
2016-04-21 14:16:02 +0530

    Add required validations in Space and SharedOfficeSpace model.

--- app/models/shared_office_space.rb
+++ app/models/shared_office_space.rb
@@ -1,4 +1,6 @@
 class SharedOfficeSpace < ActiveRecord::Base
   belongs_to :user
   belongs_to :space
+
+  validates :user, :space, presence: true
 end
--- app/models/space.rb
+++ app/models/space.rb
@@ -1,7 +1,7 @@
 class Space < ActiveRecord::Base
-
   belongs_to :owner, class_name: 'User', foreign_key: :user_id
   has_many :shared_office_spaces
   has_many :shared_users, through: :shared_office_spaces, source: :user
 
+  validates :owner, presence: true
 end
